dfd2d60d61a826ac91b6f5b355d1b8121a6682ae
Julian Mican <julian.mican@gmail.com>
2025-11-13 20:08:45 -0500

    firts test

---
+++ src/main/java/com/dreamwar/app/utils/Calculator.java
@@ -0,0 +1,7 @@
+package com.dreamwar.app.utils;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/com/dreamwar/app/utils/CalculatorTest.java
@@ -0,0 +1,15 @@
+package com.dreamwar.app.utils;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+    
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(2, 3);
+        assertEquals(5, result);
+    }
+}
