f80331b7c338f0c241a4c7c68f93126fc02ef5aa
Julian Mican <julian.mican@gmail.com>
2025-11-13 19:58:39 -0500

    firts commit

---
+++ src/main/java/com/dreamwar/app/App.java
@@ -0,0 +1,6 @@
+package com.dreamwar.app;
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to DreamWar!");
+    }
+}
---
+++ src/test/java/com/dreamwar/app/AppTest.java
@@ -0,0 +1,14 @@
+package com.dreamwar.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+    // Test methods go here
+    @Test
+    public void testApp() {
+        // Example test case
+        assertTrue(true);
+    }
+}
